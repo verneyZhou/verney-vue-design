c5d878741de30e78663026bbf577eb3a2f8107fc
zhouyuan <zhouyuan10@staff.weibo.com>
2023-12-18 00:23:52 +0800

    feat: 文档增加ai对话

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,4 @@
-import { App, Component } from 'vue'
+import { App, Component, h } from 'vue'
 import Theme from 'vitepress/theme'
 import '../styles/custom-style.css' // 对vitepress主题样式的覆盖
 import 'element-plus/dist/index.css'
@@ -7,13 +7,21 @@ import { globals } from '../components'
 
 import VerneyUI from '@verney-design/ui'
 
+
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
+
 export default {
   ...Theme,
+  // 添加Documate UI
+  Layout: h(Theme.Layout, null, {
+    'nav-bar-content-before': () => h(Documate, {
+      endpoint: 'https://4ztzgn1cxw.us.aircode.run/ask',
+    }),
+  }),
   enhanceApp ({ app }: {app: App}) {
-
     // 全局注册组件
     app.use(VerneyUI)
-
     globals.forEach((comp: Component) => {
       app.component(comp.name as string, comp)
     })
